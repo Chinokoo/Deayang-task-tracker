3459357b216db034c917d2911ab5a6efa484e549
Peter Chinoko <peterchinokoo@gmail.com>
2023-01-20 19:19:14 +0200

    my first commit, setting up the index.js

---
+++ index.js
@@ -0,0 +1,27 @@
+//modules.
+const express = require('express');
+const app = express();
+const winston = require('winston');
+const mongoose = require('mongoose');
+//const { level, info } = require('winston');
+
+//middleware functions.
+app.use(express.json());
+
+//winston.
+const logger = winston.createLogger({
+    level: 'info',
+    transports: [
+        new winston.transports.File({ filename: 'task-tracker-Log' }),
+        new winston.transports.Console()
+    ]
+});
+
+//connecting with the database.
+mongoose.connect('mongodb://127.0.0.1:27017/task-tracker')
+    .then(() => logger.info('connection to the database is successfull.'))
+    .catch(err => logger.error('connecting to the database failed.', err));
+mongoose.set('strictQuery', false);
+
+const port = process.env.PORT || 4000;
+app.listen(port, () => logger.info(`listening on port' ${port}`));
